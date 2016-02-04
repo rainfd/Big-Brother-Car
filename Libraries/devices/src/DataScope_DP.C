08d1ce64932bd9a2d639cb9726d4391d7c5af891
RainFD <rainfd@live.cn>
2016-02-04 14:19:35 +0800

    virtual scope

---
+++ Libraries/devices/src/DataScope_DP.C
@@ -0,0 +1,171 @@
+/*
+    虚拟示波器 】
+    软件：Data_Scope
+*/
+
+#include "DataScope_DP.h"
+#include "common.h"
+#include "mma7361.h"
+#include "uart.h"
+
+float scope_buf[10] = {0};        // 虚拟示波器数据
+
+unsigned char DataScope_OutPut_Buffer[42] = {0};	   //串口发送缓冲区
+
+
+//函数说明：将单精度浮点数据转成4字节数据并存入指定地址 
+//附加说明：用户无需直接操作此函数 
+//target:目标单精度数据
+//buf:待写入数组
+//beg:指定从数组第几个元素开始写入
+//函数无返回 
+void Float2Byte(float *target, unsigned char *buf,unsigned char beg)
+{
+    unsigned char *point;
+    point = (unsigned char*)target;	  //得到float的地址
+    buf[beg]   = point[0];
+    buf[beg+1] = point[1];
+    buf[beg+2] = point[2];
+    buf[beg+3] = point[3];
+}
+ 
+ 
+//函数说明：将待发送通道的单精度浮点数据写入发送缓冲区
+//Data：通道数据
+//Channel：选择通道（1-10）
+//函数无返回 
+void DataScope_Get_Channel_Data(float Data,unsigned char Channel)
+{
+	if ( (Channel > 10) || (Channel == 0) ) return;  //通道个数大于10或等于0，直接跳出，不执行函数
+  else
+  {
+     switch (Channel)
+		{
+      case 1:  Float2Byte(&Data,DataScope_OutPut_Buffer,1); break;
+      case 2:  Float2Byte(&Data,DataScope_OutPut_Buffer,5); break;
+		  case 3:  Float2Byte(&Data,DataScope_OutPut_Buffer,9); break;
+		  case 4:  Float2Byte(&Data,DataScope_OutPut_Buffer,13); break;
+		  case 5:  Float2Byte(&Data,DataScope_OutPut_Buffer,17); break;
+		  case 6:  Float2Byte(&Data,DataScope_OutPut_Buffer,21); break;
+		  case 7:  Float2Byte(&Data,DataScope_OutPut_Buffer,25); break;
+		  case 8:  Float2Byte(&Data,DataScope_OutPut_Buffer,29); break;
+		  case 9:  Float2Byte(&Data,DataScope_OutPut_Buffer,33); break;
+		  case 10: Float2Byte(&Data,DataScope_OutPut_Buffer,37); break;
+		}
+  }	 
+}
+
+
+//函数说明：生成 DataScopeV1.0 能正确识别的帧格式
+//Channel_Number，需要发送的通道个数
+//返回发送缓冲区数据个数
+//返回0表示帧格式生成失败 
+unsigned char DataScope_Data_Generate(unsigned char Channel_Number)
+{
+	if ( (Channel_Number > 10) || (Channel_Number == 0) ) { return 0; }  //通道个数大于10或等于0，直接跳出，不执行函数
+  else
+  {	
+	 DataScope_OutPut_Buffer[0] = '$';  //帧头
+		
+	 switch(Channel_Number)   
+   { 
+		 case 1:   DataScope_OutPut_Buffer[5]  =  5; return  6; break;   
+		 case 2:   DataScope_OutPut_Buffer[9]  =  9; return 10; break;
+		 case 3:   DataScope_OutPut_Buffer[13] = 13; return 14; break;
+		 case 4:   DataScope_OutPut_Buffer[17] = 17; return 18; break;
+		 case 5:   DataScope_OutPut_Buffer[21] = 21; return 22; break; 
+		 case 6:   DataScope_OutPut_Buffer[25] = 25; return 26; break;
+		 case 7:   DataScope_OutPut_Buffer[29] = 29; return 30; break;
+		 case 8:   DataScope_OutPut_Buffer[33] = 33; return 34; break;
+		 case 9:   DataScope_OutPut_Buffer[37] = 37; return 38; break;
+     case 10:  DataScope_OutPut_Buffer[41] = 41; return 42; break;
+   }	 
+  }
+	return 0;
+}
+
+void DataScope(uint32_t instance, float *scope_buf)
+{
+    unsigned char i, Send_Count;                       //通道显示数据
+    
+    DataScope_Get_Channel_Data( scope_buf[0] , 1 );  //将数据 1.0  写入通道 1
+    DataScope_Get_Channel_Data( scope_buf[1] , 2 );  //将数据 2.0  写入通道 2
+    DataScope_Get_Channel_Data( scope_buf[2] , 3 );  //将数据 3.0  写入通道 3
+    DataScope_Get_Channel_Data( scope_buf[3] , 4 );  //将数据 4.0  写入通道 4
+    DataScope_Get_Channel_Data( scope_buf[4] , 5 );  //将数据 5.0  写入通道 5
+    DataScope_Get_Channel_Data( scope_buf[5] , 6 );  //将数据 6.0  写入通道 6
+    DataScope_Get_Channel_Data( scope_buf[6] , 7 );  //将数据 7.0  写入通道 7
+    DataScope_Get_Channel_Data( scope_buf[7] , 8 );  //将数据 8.0  写入通道 8
+    DataScope_Get_Channel_Data( scope_buf[8] , 9 );  //将数据 9.0  写入通道 9
+    DataScope_Get_Channel_Data( scope_buf[9] , 10); //将数据 10.0 写入通道 10
+
+    Send_Count = DataScope_Data_Generate(10); //生成10个通道的
+    
+    for (i = 0; i < Send_Count; ++i) {
+        UART_WriteByte(instance, DataScope_OutPut_Buffer[i]);
+    }
+    
+    DelayMs(10);
+}
+
+
+unsigned short CRC_CHECK(unsigned char *Buf, unsigned char CRC_CNT)
+{
+    unsigned short CRC_Temp;
+    unsigned char i,j;
+    CRC_Temp = 0xffff;
+
+    for (i=0;i<CRC_CNT; i++){      
+        CRC_Temp ^= Buf[i];
+        for (j=0;j<8;j++) {
+            if (CRC_Temp & 0x01)
+                CRC_Temp = (CRC_Temp >>1 ) ^ 0xa001;
+            else
+                CRC_Temp = CRC_Temp >> 1;
+        }
+    }
+    return(CRC_Temp);
+}
+
+void SDS_OutPut_Data(uint32_t instance, float S_Out[])
+{
+  int temp[4] = {0};
+  unsigned int temp1[4] = {0};
+  unsigned char databuf[10] = {0};
+  unsigned char i;
+  unsigned short CRC16 = 0; 
+  float SDS_OutData[4];
+  for(i=0;i<4;i++) {
+  SDS_OutData[i]=S_Out[i];
+  }
+  for(i=0;i<4;i++)
+   {
+    
+    temp[i]  = (int)SDS_OutData[i];
+    temp1[i] = (unsigned int)temp[i];
+    
+   }
+   
+  for(i=0;i<4;i++) 
+  {
+    databuf[i*2]   = (unsigned char)(temp1[i]%256);
+    databuf[i*2+1] = (unsigned char)(temp1[i]/256);
+  }
+  
+  CRC16 = CRC_CHECK(databuf,8);
+  databuf[8] = CRC16%256;
+  databuf[9] = CRC16/256;
+  
+  for(i=0;i<10;i++)
+      UART_WriteByte(instance, databuf[i]);
+}
+
+
+
+
+
+
+
+
+
+
