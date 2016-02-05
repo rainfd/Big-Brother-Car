1e9423b7b5e65bc5be8fd0ab1b513e0d2afee5ec
RainFD <rainfd@live.cn>
2016-02-06 01:29:44 +0800

    collect data(gyro and accel) every 5ms

--- Project/K60Major/Templete/src/timer.c
+++ Project/K60Major/Templete/src/timer.c
@@ -4,15 +4,14 @@
 #include "common.h"
 //#include "adc.h"
 
-#include "mma7361.h"
-#include "l3g4200d.h"
 #include "DataScope_DP.h"
+#include "8700_2100.h"
 
 #include "filter.h"
 
 void Timer_Init(void)
 {       
-    PIT_QuickInit(HW_PIT_CH0, 5000);
+    PIT_QuickInit(HW_PIT_CH0, 5000); // 5ms周期
     
     PIT_CallbackInstall(HW_PIT_CH0, PIT_ISR); // 注册PIT 中断回调函数 0号定时器的中断处理
     PIT_ITDMAConfig(HW_PIT_CH0, kPIT_IT_TOF, true); // 开启PIT0定时器中断
@@ -22,24 +21,31 @@ float xi = 0, yi = 0, zi = 0;
 
 void PIT_ISR(void)
 {
-//    int16_t gyroaccel[6];
-//    
-//    GyroAccel_Read(gyroaccel);
-    
-    
-    
-#if 0
 
-    yi += dps.y * 0.005;
     
-    scope_buf[0] = rot.pitch;
-    scope_buf[1] = dps.y;
-//    
-    scope_buf[6] = yi;
-//    
-    scope_buf[7] = Kalman_Filter(rot.pitch, dps.y);	
-    scope_buf[8] = Complementary_Filter(rot.pitch, dps.y);
-    scope_buf[9] = Complementary_Filter2(rot.pitch, dps.y);
+//    int16_t gyro_x = gyroaccel[0], gyro_y = gyroaccel[1], gyro_z =  gyroaccel[2],
+//            accel_x = gyroaccel[3], accel_y = gyroaccel[4], accel_z = gyroaccel[5];
+    
+    
+#if 1
+
+    float gyroaccel[6];
+    
+    GyroAccel_Read(gyroaccel);
+        
+    scope_buf[0] = gyroaccel[0];
+    scope_buf[1] = gyroaccel[1];
+    scope_buf[2] = gyroaccel[2];
+    scope_buf[3] = gyroaccel[3];
+    scope_buf[4] = gyroaccel[4];
+    scope_buf[5] = gyroaccel[5];
+ 
+//    scope_buf[6] = yi;
+ 
+    scope_buf[7] = Kalman_Filter(gyroaccel[5], gyroaccel[1]);	
+    scope_buf[8] = Complementary_Filter(gyroaccel[5], gyroaccel[1]);
+    scope_buf[9] = Complementary_Filter2(gyroaccel[5], gyroaccel[1]);
+    
 #endif
 
 }
