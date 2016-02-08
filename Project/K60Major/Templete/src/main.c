36ce79abedbeeec81a7d2a47b1042eadc2044648
RainFD <rainfd@live.cn>
2016-02-09 05:39:37 +0800

    show the counter on the LED

--- Project/K60Major/Templete/src/main.c
+++ Project/K60Major/Templete/src/main.c
@@ -3,6 +3,7 @@
 #include "uart.h"
 #include "adc.h"
 #include "pit.h"
+#include "ftm.h"
 
 #include "LQ_OLED.h"
 #include "DataScope_DP.h"
@@ -11,16 +12,12 @@
 #include "counter.h"
 #include "filter.h"
 #include "timer.h"
+#include "motor.h"
 
 #define LED_ON PEout(26) = 0
 #define LED_OFF PEout(26) = 1
 
 uint32_t uart_instance;
-    
-extern uint32_t ChlValue[2];
-
-Data fin;
-
 
 // Turn = Gyro_y;
 // Balance = Gyro_x, accel_y
@@ -34,23 +31,32 @@ int main(void)
     uart_instance = UART_QuickInit(UART4_RX_PE25_TX_PE24, 115200);
     
     LCD_Init();
-    LCD_Print(20, 2, "Big Brother");
+    LCD_Print(20, 0, "Big Brother");
     
 //    LED_ON;  // 查看初始化时间
 //
-//    LED_ON;   // 
+    LED_ON;   
     
     GyroAccel_Init();
     
     CounterInit();
     Timer_Init();
+    PWM_Init(5000);
+    
+    printf("Init OK");
     
     while(1)
     {
 
 //        printf("[CH%d]:%4dHz [CH%d]:%4dHz\r\n", 0, ChlValue[0], 1, ChlValue[1]);
-
+        LCD_Print(0, 2, "L: ");
+        LCD_Print_Num(20, 2, ChlValue[0], 5);
+        LCD_Print(0, 4, "R: ");
+        LCD_Print_Num(20, 4, ChlValue[1], 5);
+        
+        
         DataScope(uart_instance, scope_buf);
+        
 #if 0
     float gyroaccel[6];
     
