a6b702200dccc7ce084e97e87c5fd74c432d2cb9
RainFD <rainfd@live.cn>
2016-02-18 17:50:37 +0800

    funtion name modify

--- Project/K60Major/Templete/inc/calculate.h
+++ Project/K60Major/Templete/inc/calculate.h
@@ -1,7 +1,8 @@
 #ifndef _CALCULATE_H
 #define _CALCULATE_H
 
-float AngelCal(int angle, int gyro, int way);
+float AngleCal(int raw, int way);
+float AngleFilter(float angle, float gyro, int way);
 int Balance_PID(float angle, float gyro);
 int Speed_PID(int encoder_l, int encoder_r);
 int Turn_PID(int encoder_l, int encoder_r, float gyro);
